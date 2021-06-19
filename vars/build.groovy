b4ef0a599157af519f07a540f2257439927231c0
Kumar90-pintu <81384095+Kumar90-pintu@users.noreply.github.com>
2021-06-20 01:17:38 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,5 @@
 def call(version){
   //sh 'git clone https://github.com/Kumar90-pintu/Simpletest.git'
   sh 'ls -l'
-  sh "docker build . -t test:${version} -f Simpletest/Dockerfile"
+  sh "docker build . -t test:${version} -f Simpletest/."
 }
