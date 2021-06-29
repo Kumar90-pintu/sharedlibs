59b658dfb946b2650391f109fdce26f313b86e91
root <root@ip-10-0-152-95.ap-south-1.compute.internal>
2021-06-29 19:21:09 +0000

    added new changes push2

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -2,6 +2,8 @@ def call(value) {
         try {
             stage("Clonning codebase in workspace") {
                 //checkout scm
+                echo "cleaning old clone'
+                sh " rm -rf $value "
                 echo 'pulling'
                def url = "https://github.com/Kumar90-pintu"
                sh " git clone $url/$value "
